88d110a61face683f68143189531212aba3f6554
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-22 16:42:07 +0200

    Controller.php - Create common render function

---
+++ app/core/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+class Controller
+{
+    /**
+     * Renders views with specified data
+     * @param string $view
+     * @param string $layout
+     * @param mixed $page
+     * @param array $data
+     * @return never
+     */
+    protected function view(string $view, string $layout, ?string $page = null, array $data = []): never
+    {
+        extract($data);
+        require_once "app/views/{$view}/layouts/{$layout}.php";
+        exit();
+    }
+}
