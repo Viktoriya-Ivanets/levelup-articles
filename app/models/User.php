5fd0d8abb4da0383a8c67ae9d35e174fa6586157
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-25 10:34:05 +0200

    User.php - Add function to find user in DB by login

--- app/models/User.php
+++ app/models/User.php
@@ -6,4 +6,16 @@ class User extends Model
     {
         parent::__construct('users');
     }
+
+    /**
+     * Get all needed user's data from DB by login
+     * @param string $login
+     * @return array|null
+     */
+    public function findUserByLogin(string $login): ?array
+    {
+        $query = "SELECT id, login, password FROM users WHERE login = ?";
+        $result = $this->fetchOne($query, 's', [$login]);
+        return $result ?? null;
+    }
 }
