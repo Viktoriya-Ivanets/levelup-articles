764cdef3164d744a0f8ad426696e50eb63fef592
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-25 15:12:50 +0200

    Helpers.php - Create function for filtering post data

--- app/utils/Helpers.php
+++ app/utils/Helpers.php
@@ -25,4 +25,21 @@ class Helpers
         $dateTime->setTimezone(new DateTimeZone(date_default_timezone_get()));
         return $dateTime->format('Y-m-d H:i:s');
     }
+
+    /**
+     * Filters and returns array of data
+     * @param array $fields
+     * @return array
+     */
+    public static function getPostData(array $fields): array
+    {
+        $postData = [];
+
+        foreach ($fields as $field) {
+            $value = filter_input(INPUT_POST, $field, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $value !== null && $value !== false ? $postData[$field] = $value : $postData[$field] = null;
+        }
+
+        return $postData;
+    }
 }
